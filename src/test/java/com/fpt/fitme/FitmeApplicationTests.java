ea42dbdddad59a4680b26907d24ac8bafe81c2c3
Trần Cẩm Long <camlong.tran0702@gmail.com>
2021-06-18 18:34:24 +0700

    Try to deploy on Heroku

--- src/test/java/com/fpt/fitme/FitmeApplicationTests.java
+++ src/test/java/com/fpt/fitme/FitmeApplicationTests.java
@@ -1,15 +1,10 @@
 package com.fpt.fitme;
 
-import com.fpt.fitme.repository.AppUserRoleRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FitmeApplicationTests {
-
-	@Autowired
-	private AppUserRoleRepository appUserRoleRepository;
 
 	@Test
 	void contextLoads() {
