ea019ed13810950cf34b00aed291f77667ff101a
Trần Cẩm Long <camlong.tran0702@gmail.com>
2021-06-18 18:03:14 +0700

    Init App User Repo

--- src/main/java/com/fpt/fitme/config/WebSecurityConfig.java
+++ src/main/java/com/fpt/fitme/config/WebSecurityConfig.java
@@ -5,6 +5,9 @@ import com.fpt.fitme.service.FitmeUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -15,11 +18,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public String crossOriginAllowedHeaders="header1,header2, *" ;
+    public String crossOriginAllowedSites="site1,site2, * ";
 
     @Autowired
     private FitmeUserDetailsService fitmeUserDetailsService;
@@ -52,5 +63,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    protected CorsFilter crossFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.setAllowedHeaders(Arrays.asList(crossOriginAllowedHeaders.split(",")));
+        config.setAllowedOrigins(Arrays.asList(crossOriginAllowedSites.split(",")));
+        //config.setAllowedHeaders("*"); whitelist all sites
+        //config.setAllowedOrigins("*"); whitelist all headers
+        config.addAllowedMethod(HttpMethod.OPTIONS);
+        config.addAllowedMethod(HttpMethod.GET);
+        config.addAllowedMethod(HttpMethod.POST);
+        config.addAllowedMethod(HttpMethod.PUT);
+        config.addAllowedMethod(HttpMethod.DELETE);
+        config.addExposedHeader("Authorization");
+        config.setMaxAge(new Long(1800));
+
+        source.registerCorsConfiguration("/api/**", config);
+        source.registerCorsConfiguration("/v2/api-docs", config);
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+
 
 }
