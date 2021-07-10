1ec919aff552f35ed6b74bc9412ed817091f9a46
Ngô Tấn Đức <ngotanduc410@gmail.com>
2021-07-10 13:08:56 +0700

    Fix trainee controller

--- src/main/java/com/fpt/fitme/controller/TraineeController.java
+++ src/main/java/com/fpt/fitme/controller/TraineeController.java
@@ -124,7 +124,8 @@ public class TraineeController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return new ResponseEntity<>(null, HttpStatus.OK);
-      
+    }
+
     @PostMapping("favorite/{type}/{id}")
     public ResponseEntity<?> favoriteItem(@PathVariable String type, @PathVariable long id) {
         try {
